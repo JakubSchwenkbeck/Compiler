f2e9c4c315140fd03f030234fca0ab87300d9623
Jakub <jakub@schwenkbeck.de>
2024-10-24 22:17:31 +0200

    Full implemented lexer for a Java Subset

--- Blatt2/app/Main.hs
+++ Blatt2/app/Main.hs
@@ -4,6 +4,28 @@ import qualified Scanner
 
 main :: IO ()
 main = do
-  print (Scanner.lexer "class public int 76")
+  putStrLn "\n"
+  let string1 = "public static void main { }"
+  putStrLn "The first input :" 
+  print string1
 
+  print $ Scanner.lexer string1
+
+
+  let string2 = "private int VariableName = 200"
+  putStrLn "\n The second input :"
+  print string2
+  print $ Scanner.lexer string2
+
+ 
+  let string3 = "public static void main { public boolean b = true    private int Var = 1337 b + Var return 5}"
+  putStrLn "\n The third input : "
+  print string3
+  print $ Scanner.lexer string3
+
+  let string4 = "This is an error Test $$ §§ ``"
+  putStrLn "\n This is an test for error / unexpected behavior"
+  print string4
+  print $ Scanner.lexer string4
+ 
  
